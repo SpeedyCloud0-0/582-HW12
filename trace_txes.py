7c669e1b5365c449fb196a7e822ff70deb5d32b6
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-08-03 22:25:35 -0700

    test

--- trace_txes.py
+++ trace_txes.py
@@ -46,6 +46,8 @@ class TXO:
 
     def get_inputs(self, d=1):
         tx = rpc_connection.getrawtransaction(self.tx_hash, True)
+        if tx['vout'] == "":
+            return
         for i in range(d):
             output = tx['vout'][i]
             tx_obj = TXO(tx_hash=self.tx_hash, n=i, amount=output['value'] * pow(10, 8),
