99bdab6ee2e1c80cc93ec5188107597ade90f78c
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-08-03 21:50:20 -0700

    test

--- trace_txes.py
+++ trace_txes.py
@@ -2,17 +2,18 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import json
 from datetime import datetime
 
-rpc_user='quaker_quorum'
-rpc_password='franklin_fought_for_continental_cash'
-rpc_ip='3.134.159.30'
-rpc_port='8332'
+rpc_user = 'quaker_quorum'
+rpc_password = 'franklin_fought_for_continental_cash'
+rpc_ip = '3.134.159.30'
+rpc_port = '8332'
 
-rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, rpc_ip, rpc_port))
+rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s" % (rpc_user, rpc_password, rpc_ip, rpc_port))
+
 
 ###################################
 
 class TXO:
-    def __init__(self, tx_hash, n, amount, owner, time ):
+    def __init__(self, tx_hash, n, amount, owner, time):
         self.tx_hash = tx_hash
         self.n = n
         self.amount = amount
@@ -21,26 +22,32 @@ class TXO:
         self.inputs = []
 
     def __str__(self, level=0):
-        ret = "\t"*level+repr(self.tx_hash)+"\n"
+        ret = "\t" * level + repr(self.tx_hash) + "\n"
         for tx in self.inputs:
-            ret += tx.__str__(level+1)
+            ret += tx.__str__(level + 1)
         return ret
 
     def to_json(self):
-        fields = ['tx_hash','n','amount','owner']
-        json_dict = { field: self.__dict__[field] for field in fields }
-        json_dict.update( {'time': datetime.timestamp(self.time) } )
+        fields = ['tx_hash', 'n', 'amount', 'owner']
+        json_dict = {field: self.__dict__[field] for field in fields}
+        json_dict.update({'time': datetime.timestamp(self.time)})
         if len(self.inputs) > 0:
             for txo in self.inputs:
-                json_dict.update( {'inputs': json.loads(txo.to_json()) } )
+                json_dict.update({'inputs': json.loads(txo.to_json())})
         return json.dumps(json_dict, sort_keys=True, indent=4)
 
     @classmethod
-    def from_tx_hash(cls,tx_hash,n=0):
-        pass
-        #YOUR CODE HERE
+    def from_tx_hash(cls, tx_hash, n=0):
+        tx = rpc_connection.getrawtransaction(tx_hash, True)
+        tx_n = tx[n]
+        tx_obj = TXO(tx_hash=tx_hash, n=n, amount=tx_n['vout']['value'] * 10 ^ 8,
+                     owner=tx_n['vout']['addresses']['address'], time=datetime.fromtimestamp(tx_n['time']))
+        return tx_obj
 
-    def get_inputs(self,d=1):
-        pass
-        #YOUR CODE HERE
-
+    def get_inputs(self, d=1):
+        for i in range(d):
+            tx = rpc_connection.getrawtransaction(self.tx_hash, True)
+            tx_n = tx[i]
+            tx_obj = TXO(tx_hash=self.tx_hash, n=n, amount=tx_n['vout']['value'] * 10 ^ 8,
+                         owner=tx_n['vout']['addresses']['address'], time=datetime.fromtimestamp(tx_n['time']))
+            self.inputs.append(tx_obj)
